0545585726b608011d6fb22868072a0afb412ac8
Süveges <suveges.zoltan@petrik.hu>
2022-12-14 13:57:05 +0100

    kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
-import {
-  Controller,
-  Get,
-  Render,
-} from '@nestjs/common';
+import { Controller, Get, Render, Body, Redirect, Query, Post} from '@nestjs/common';
+import { syncBuiltinESMExports } from 'module';
 import { AppService } from './app.service';
+import { CatsDto } from './cats.dto';
 import db from './db';
 
 @Controller()
@@ -12,7 +10,46 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index() {
+    const [rows] = await db.execute(
+      'SELECT * FROM macskak ORDER BY suly'
+    );
+
+    return {
+      cats: rows,
+    };
   }
+
+  @Get('search')
+  @Render('index')
+  async search(@Query('eyeColor') eyeColor) {
+    const [rows] = await db.execute(
+      'SELECT * FROM macskak WHERE szem_szin LIKE ? ORDER BY suly',
+      ['%' + eyeColor + '%']
+    );
+
+    return {
+      cats: rows,
+    }
+  }
+
+  @Get('cats/new')
+  @Render('addCat')
+  newCatFoprm(){
+    return {};
+  }
+
+  @Post('cats/new')
+  @Redirect()
+  async newCat(@Body() cat: CatsDto){
+    const [result] = await db.execute(
+      'INSERT INTO macskak (suly, szem_szin) VALUES (?, ?)',
+      [cat.weight, cat.eyeColor]
+    );
+
+    return {
+      url: "/",
+    };
+  }
+
 }
